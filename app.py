23a644e680ff5707e5d334d3422a347f2ad19056
ksswaroop <k.saiswaroop@hotmail.com>
2023-10-14 04:13:01 +0530

    Commiting "First chagnes"

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route("/")
-
-def hello_jovian():
+def hello_world():
     return render_template('home.html')
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
+
 
